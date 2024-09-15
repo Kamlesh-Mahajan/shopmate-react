9ed3a1531e66923a981a7a6b0cd891b17ab2d2c0
Kamlesh-Mahajan <kamleshmahajan545@gmail.com>
2024-09-15 18:46:01 +0530

    Refactor Header component and CSS styles and  also added themes for customization

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import { useCart } from "../context/CartContext";
 import Logo from "../assets/favicon.ico";
@@ -6,6 +7,16 @@ import Logo from "../assets/favicon.ico";
 import "./Header.css";
 
 export const Header = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "light"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
   const { cartList } = useCart();
 
   return (
@@ -27,6 +38,18 @@ export const Header = () => {
       <Link to="/cart" className="items">
         <span>Cart: {cartList.length}</span>
       </Link>
+
+      <div className="themeSelector">
+        <span
+          onClick={() => setTheme("light")}
+          className={theme === "light" ? "light activeTheme" : "light"}
+        ></span>
+
+        <span
+          onClick={() => setTheme("gTwo")}
+          className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}
+        ></span>
+      </div>
     </header>
   );
 };
